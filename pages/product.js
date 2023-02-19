4a87c5bba39bcbae25e37edbd7bddd74cf5c01d3
Isabela <r_isabela@outlook.com>
2023-02-19 19:20:28 +0000

    criando a pagina do produto

---
+++ pages/product.js
@@ -0,0 +1,83 @@
+import {
+  Avatar,
+  Box,
+  Card,
+  CardHeader,
+  CardMedia,
+  Chip,
+  Container,
+  Grid,
+  Typography 
+} from '@material-ui/core'
+import TemplateDefault from '../src/templates/Default'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    backgroundColor: theme.palette.background.white,
+    padding: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+  productName: {
+    margin: '15px 0',
+  },
+  price: {
+    fontWeight: 'bold',
+    marginBottom: 15,
+  }
+}))
+
+const Product = () => {
+  const classes = useStyles()
+
+  return (
+    <TemplateDefault>
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <Box className={classes.box}>
+              Carrossel
+            </Box>
+
+            <Box className={classes.box} textAlign="left">
+              <Typography component="span" variant="caption">Publicado 16 junhode 2021</Typography>
+              <Typography component="h4" variant="h4" className={classes.productName}>Jaguar XE 2.0 D R-Sport Aut.</Typography>
+              <Typography component="h4" variant="h4" className={classes.price}>R$ 50.000,00</Typography>
+              <Chip label="Categoria" />
+            </Box>
+
+            <Box className={classes.box} textAlign="left">
+              <Typography component="h6" variant="h6">Descrição</Typography>
+              <Typography component="p" variant="body2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Typography>
+            </Box>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Card elevation={0} className={classes.box}>
+              <CardHeader 
+                avatar={
+                  <Avatar>T</Avatar>
+                }
+                title="Isabela Ribeiro"
+                subheader="isabela@email.com"
+              />
+              <CardMedia 
+                image="http://source.unsplash.com/random"
+                title="Isabela Ribeiro"
+              />
+            </Card>
+
+            <Box className={classes.box}>
+              <Typography component="h6" variant="h6">Localização</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </TemplateDefault>
+  )
+}
+
+export default Product
