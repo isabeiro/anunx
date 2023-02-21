eed9997b0ff2b779c738e20a32d2cafa7afd46cd
Isabela <r_isabela@outlook.com>
2023-02-21 15:20:42 +0000

    api post users + password crypto

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,6 @@
 import dbConnect from '../../src/utils/dbConnect'
+import { crypto } from '../../src/utils/password'
+import UsersModel from '../../src/models/users'
 
 const users = async (req, res) => {
   const { method } = req
@@ -8,6 +10,32 @@ const users = async (req, res) => {
       await dbConnect()
       res.status(200).json({ success: true })
       break
+
+    case 'POST':
+      // pegar os dados que vem no req
+      // conectar no banco
+      // criptografar a senha
+      // salvar os dados
+      // responder sucesso
+      const {
+        name,
+        email,
+        password,
+      } = req.body
+
+      await dbConnect()
+
+      const passwordCrypto = await crypto(password)
+
+      const user = new UsersModel({
+        name,
+        email,
+        password: passwordCrypto,
+      })
+
+      user.save()
+
+      res.status(201).json({ success: true })
   }
 }
 
